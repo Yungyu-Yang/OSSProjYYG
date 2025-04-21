e8f92786e946a32b75d6e7045768182665134183
skiiNlr <khn6450@dgu.ac.kr>
2025-04-21 18:08:32 +0900

    add : project setting

---
+++ src/main/java/com/skycastle/mindtune/MindtuneApplication.java
@@ -0,0 +1,13 @@
+package com.skycastle.mindtune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindtuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindtuneApplication.class, args);
+	}
+
+}
