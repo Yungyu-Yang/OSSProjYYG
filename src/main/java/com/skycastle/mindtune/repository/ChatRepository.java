b5a5f56bdc3e23a072ba1f92df9a6163c87d9593
pro_1030 <2022110463@dgu.ac.kr>
2025-05-03 03:02:42 +0900

    황서영/250503/채팅 기능 추가

--- src/main/java/com/skycastle/mindtune/repository/ChatRepository.java
+++ src/main/java/com/skycastle/mindtune/repository/ChatRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
 
 public interface ChatRepository extends JpaRepository<ChatEntity, Long> {
 
