084e0888c1a85474400d8705f444276d03da2f85
skiiNlr <khn6450@dgu.ac.kr>
2025-05-25 15:48:36 +0900

    김하늘/250525/music 수정

--- src/main/java/com/skycastle/mindtune/repository/DayMusicRepository.java
+++ src/main/java/com/skycastle/mindtune/repository/DayMusicRepository.java
@@ -2,6 +2,8 @@ package com.skycastle.mindtune.repository;
 
 import com.skycastle.mindtune.entity.DayMusicEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -9,5 +11,12 @@ import java.util.List;
 
 public interface DayMusicRepository extends JpaRepository<DayMusicEntity, Long> {
     List<DayMusicEntity> findByUnoAndCreatedAtBetween(Long uno, LocalDateTime start, LocalDateTime end);
-    List<DayMusicEntity> findByUnoAndCreatedAtOrderByCreatedAtDesc(Long uno, LocalDate createdAt);
-} 
+//    List<DayMusicEntity> findByUnoAndCreatedAtOrderByCreatedAtDesc(Long uno, LocalDate createdAt);
+    @Query("SELECT d FROM DayMusicEntity d WHERE d.uno = :uno AND d.createdAt BETWEEN :start AND :end ORDER BY d.createdAt DESC")
+    List<DayMusicEntity> findByUnoAndCreatedAtBetweenOrderByCreatedAtDesc(
+            @Param("uno") Long uno,
+            @Param("start") LocalDateTime start,
+            @Param("end") LocalDateTime end
+    );
+
+}
